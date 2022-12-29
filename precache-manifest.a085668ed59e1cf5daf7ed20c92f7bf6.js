1e2358ec40831b247f8c281d85d5d38e45df907e
Code Sandbox <hello@codesandbox.io>
2022-12-29 12:50:46 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a085668ed59e1cf5daf7ed20c92f7bf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ea391e4b0c338140bf2b36188713a5",
+    "url": "https://.github.io/csb-nz63st/index.html"
+  },
+  {
+    "revision": "5ee6aca39afaac40f096",
+    "url": "https://.github.io/csb-nz63st/static/css/main.de272f7b.chunk.css"
+  },
+  {
+    "revision": "ebda7ccf87c6c48d3390",
+    "url": "https://.github.io/csb-nz63st/static/js/2.e83e2d1c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://.github.io/csb-nz63st/static/js/2.e83e2d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee6aca39afaac40f096",
+    "url": "https://.github.io/csb-nz63st/static/js/main.601ad780.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://.github.io/csb-nz63st/static/js/main.601ad780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c879be7b6f2f0eb318",
+    "url": "https://.github.io/csb-nz63st/static/js/runtime-main.ea1ef4ef.js"
+  }
+]);
